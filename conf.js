24e4bd9bbc597f10d9c52319661bead8f063ebde
Harsh <harshsheetal18@gmailcom>
2017-09-10 11:45:05 -0400

    added new file

---
+++ conf.js
@@ -0,0 +1,16 @@
+exports.config = {
+ directConnection: true,
+
+
+capabilities: {
+  'browserName' : 'chrome'
+},
+
+
+
+//framework: 'jasmine',
+
+
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['user_AbleToValidateDestinationInputBox.js']
+};
