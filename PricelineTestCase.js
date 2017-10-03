362ac409e6ce3a2159b0ea8a998429693f2c5be3
Harsh <harshsheetal18@gmail.com>
2017-10-03 17:55:43 -0400

    Added new code

--- PricelineTestCase.js
+++ PricelineTestCase.js
@@ -20,9 +20,28 @@ browser.actions().mouseMove(elem).click().perform();*/
 /*var search_btn= element(by.css("(//button[contains(@class,'green')])[1]"));
 browser.wait(EC.visibilityOf(search_btn), 5000);
 search_btn.click();*/
+
 element(by.css('.hotel-retail-search-form .primary.green')).sendKeys(protractor.Key.TAB);
 element(by.css('.hotel-retail-search-form .primary.green')).click();
+//browser.wait(EC.urlContains('stay/search'), 10000);
 browser.sleep(10000);
+
+//element(by.xpath(".//*[@id='search-retail-listings']/div/div/div[1]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[2]/div/a/div/p")).sendKeys(protractor.Key.TAB);
+element(by.xpath(".//*[@id='search-retail-listings']/div/div/div[1]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[2]/div/a/div/p")).click();
+browser.sleep(10000);
+
+browser.getAllWindowHandles().then(function(handles) {
+      browser.switchTo().window(handles[1]);
+      browser.sleep(10000);
+})
+
+element(by.xpath(".//*[@id='details-rate-selector']/div/div[5]/div[2]/div[2]/div/div/table/tbody/tr/td[2]/div/div/div[3]/button")).sendKeys(protractor.Key.TAB);
+element(by.xpath(".//*[@id='details-rate-selector']/div/div[5]/div[2]/div[2]/div/div/table/tbody/tr/td[2]/div/div/div[3]/button")).click();
+//browser.pause();
+//browser.wait(EC.urlContains('checkout'), 5000);
+browser.sleep(15000);
+
+//browser.wait(EC.urlContains('checkout'), 5000);
 
 done();
   })
